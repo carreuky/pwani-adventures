f2dc57a59fe9a92d056c43652dcb1d60cdef7941
kelvin <carreuky@gmail.com>
2023-09-16 10:46:55 +0300

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Home from './pages/Home'
 import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
     <section className=''>
     <NavBar/>
     <Home/>
+    <Footer/>
     </section>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,67 @@
-t
+import React from "react";
+import footerlogo from "../assets/flogo.png";
+
+export default function Footer() {
+  const d = new Date();
+  let year = d.getFullYear();
+  return (
+    <footer className="bg-[#F47E3B] px-4">
+      <div className="flex">
+        <div className="w-16">
+          <img class="  object-cover" src={footerlogo} alt="avatar" />
+        </div>
+        <div
+          className="navbar w-full text-xs font-bold
+                 flex   flex-wrap items-center justify-end md:gap-x-7 md:mt-0 md:pl-7 "
+        >
+          <a className="text-white pr-3 px-3" aria-current="page" href="#">
+            HOME
+          </a>
+
+          <a className="text-white pr-3 px-3" href="#">
+            ABOUT
+          </a>
+
+          <a className="text-white pr-3" href="#">
+            SERVICES
+          </a>
+          <a className="text-white pr-3" href="#">
+            CONTACT US
+          </a>
+        </div>
+      </div>
+      <hr className="text-xs" />
+
+      <div class="flex flex-col-reverse justify-between  lg:flex-row py-3">
+        <p className="text-xs text-white ">Copyright {year} Carreuky</p>
+
+        <ul class="flex flex-col mb-3 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
+          <li>
+            <a
+              href="/"
+              class="text-sm text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+            >
+              F.A.Q
+            </a>
+          </li>
+          <li>
+            <a
+              href="/"
+              class="text-sm text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+            >
+              Privacy Policy
+            </a>
+          </li>
+          <li>
+            <a
+              href="/"
+              class="text-sm text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+            >
+              Terms &amp; Conditions
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
